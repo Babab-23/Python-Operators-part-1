6d15c640b4777614201eff43e0c6dbf10ba57e6b
Babab-23 <adrianidialu@gmail.com>
2024-12-08 20:34:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Storing Values
+tree1= 24
+tree2= 35
+tree3= 54
+tree4= 69
+tree5= 16
+
+# Finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of the total of all the 5 trees is: ", sum)
+
+# Finding the average of trees
+average= sum/5
+print("The average of all the tree:",average)
